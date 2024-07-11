746c991b69b406d9bcff212e455325b865c0dcdd
Ajay <ajayrajnegi111@gmail.com>
2024-07-11 10:36:13 +0530

    quite a few changes

---
+++ src/components/postCard/PostCard.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import styles from "./postCard.module.css";
+import Link from "next/link";
+
+const PostCard = () => {
+  return (
+    <>
+      {/* <div className={styles.container}>
+        <div className={styles.top}>
+          {post.img && (
+            <div className={styles.imgContainer}>
+              <Image src={post.img} alt="" fill className={styles.img} />
+            </div>
+          )}
+          <span className={styles.date}>
+            {post.createdAt?.toString().slice(4, 16)}
+          </span>
+        </div>
+        <div className={styles.bottom}>
+          <h1 className={styles.title}>{post.title}</h1>
+          <p className={styles.desc}>{post.body}</p>
+          <Link className={styles.link} href={`/blog/${post.slug}`}>
+            READ MORE
+          </Link>
+        </div>
+      </div> */}
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <div className={styles.imgContainer}>
+            <Image
+              src="/post.png"
+              width={100}
+              height={100}
+              className={styles.img}
+            />
+          </div>
+          <span className={styles.date}>16-08-03</span>
+        </div>
+        <div className={styles.bottom}>
+          <h1 className={styles.title}></h1>
+          <p className={styles.desc}>Desc</p>
+          <Link className={styles.link} href="/blog/post">
+            READ MORE
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostCard;
